2a60c3a88dcaf8a8204377a72d75cec1969375a3
LucasHSKKK <poqueni50@gmail.com>
2024-05-24 14:38:35 +0200

    new code

---
+++ main.py
@@ -0,0 +1,14 @@
+def roman_to_int(roman):
+ roman_numerals = {"I": 1, "V": 5, "X": 10, "C": 100, "D": 500, "M": 1000}
+ roman_number = input("Insert a Roman number to convert: ")
+ 
+
+
+def int_to_roman(int):
+  # complete code here
+
+print(" ---------- Roman Numberals Conversion ----------")  
+print(" > 1 - Convert an integer to a roman numerals,")  
+print(" > 2 - Convert a roman numerals to an integer.")  
+option = input("Choose an option (1 or 2):")  
+# complete code here
