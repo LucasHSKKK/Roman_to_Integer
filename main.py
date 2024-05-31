5fb4d174c761fa6c1c47eb1ed40f9b4116148a0c
LucasHSKKK <poqueni50@gmail.com>
2024-05-31 21:22:16 +0200

    some lines but still not working

--- main.py
+++ main.py
@@ -1,14 +1,39 @@
-def roman_to_int(roman):
- roman_numerals = {"I": 1, "V": 5, "X": 10, "C": 100, "D": 500, "M": 1000}
- roman_number = input("Insert a Roman number to convert: ")
- 
+def roman_to_int():
+    roman_numerals_less = {
+        "I": 1,
+        "X": 10,
+        "C": 100,
+    }
+    roman_numerals_normal = {
+        "V": 5,
+        "D": 500,
+        "M": 1000,
+    }
+    number_organized = {}
+    sequence = input("Insert a Roman number to convert: ")
+    sequence = sequence.upper()
+    for letter in sequence:
+        # fix this line
+        if letter not in roman_numerals_less or not roman_numerals_normal:
+            print("Incorrect value, try again.")
+            roman_to_int()
+        else:
+            print("correct")
 
 
-def int_to_roman(int):
-  # complete code here
+def int_to_roman():
+    print()
 
-print(" ---------- Roman Numberals Conversion ----------")  
-print(" > 1 - Convert an integer to a roman numerals,")  
-print(" > 2 - Convert a roman numerals to an integer.")  
-option = input("Choose an option (1 or 2):")  
-# complete code here
+
+def choose():
+    print(" ---------- Roman Numberals Conversion ----------")
+    print(" > 1 - Convert an integer to a roman numerals,")
+    print(" > 2 - Convert a roman numerals to an integer.")
+    option = input("Choose an option (1 or 2): ")
+    if option == "1":
+        roman_to_int()
+    else:
+        int_to_roman()
+
+
+choose()
